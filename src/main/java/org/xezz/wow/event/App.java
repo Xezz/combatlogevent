0330f2c5eb8e8d9733193a6e5c5c9344b4a85f12
Xezz <koch@xezz.org>
2013-11-08 00:36:48 +0100

    Initial commit

---
+++ src/main/java/org/xezz/wow/event/App.java
@@ -0,0 +1,13 @@
+package org.xezz.wow.event;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
