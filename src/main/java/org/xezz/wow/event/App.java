8b98177b68a3677cbbfdb1d4fb2c775c563ec8aa
Xezz <koch@xezz.org>
2013-11-08 00:42:00 +0100

    UPDATE: Java7 and updated dependencies for maven

--- src/main/java/org/xezz/wow/event/App.java
+++ src/main/java/org/xezz/wow/event/App.java
@@ -2,12 +2,9 @@ package org.xezz.wow.event;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
